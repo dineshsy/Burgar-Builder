c0d421c9b16995fb90e97610b0d75391de6116ad
Dinesh <s.ydinesh1177@gmail.com>
2020-03-18 23:38:22 +0530

    Navigation Items in toolbar

--- burger-builder/src/components/ToolBar/Toolbar.js
+++ burger-builder/src/components/ToolBar/Toolbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import classes from "./Toolbar.module.css";
 import Logo from '../LOGO/Logo';
+import NavigationItems from '../Navigation/NavigationItems'
 
 const toolbar = props => (
     <header className = {classes.Toolbar}>
         <div>MENU</div>
         <Logo/>
-        <div>nav</div>
+        <NavigationItems/>
     </header>
 )
 
